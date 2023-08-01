14c8ababd94fec985a9ac570d9d5761587c5047a
jimmydaniel <mamiye05@gmail.com>
2023-08-01 11:02:57 +0000

    Create a function named setFromArray that returns a Set from an array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Leo Azubuike <https://github.com/Azubuikeleo13>
+ * @returns {Set}
+ */
+
+export default function setFromArray(array) {
+  return new Set(array);
+}
